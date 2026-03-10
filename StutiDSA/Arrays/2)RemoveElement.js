299b7c2e6c739a2128ef6b6914b4b48545172dcd
stutisk <48529714+stutisk@users.noreply.github.com>
2026-03-10 21:48:07 +0530

    added remove elmenet

---
+++ StutiDSA/Arrays/2)RemoveElement.js
@@ -0,0 +1,11 @@
+var removeElement = function(array, val) {
+    let i=0;
+    for(let j = 0; j< array.length;j++ ){
+        if(array[j] !== val){
+            array[i] = array[j];
+            i++;
+
+        }
+    }
+    return i+1; 
+};
--- StutiDSA/Arrays/RemoveDuplicates.js
+++ StutiDSA/Arrays/RemoveDuplicates.js
@@ -1,6 +1,8 @@
 var removeDuplicates = function (array) {
   if (array.length === 0) return false;
-  let i = 0;
+  let i = 0; 
+  // track result
+  // array traverse
   for (let j = i + 1; j < array.length; j++) {
     if (array[i] !== array[j]) {
       i++;
@@ -11,6 +13,10 @@ var removeDuplicates = function (array) {
 };
 
 console.log(removeDuplicates([1, 1, 2, 2, 3, 4, 4]));
+
+
+
+[1,2,3,4]
 let arr = [1, 1, 2, 2, 3, 4, 4];
 let k = removeDuplicates(arr);
 console.log(arr.slice(0,k));
