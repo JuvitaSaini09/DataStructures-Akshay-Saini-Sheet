565e49857186fe6d4990175c861c1a3f741ab9c6
JuvitaSaini <juvita.saini@nimbusnext.com>
2026-03-14 23:31:02 +0530

    single number sol using map

--- JuvitaDSA/Arrays/9singleNumber.js
+++ JuvitaDSA/Arrays/9singleNumber.js
@@ -1,3 +1,7 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -10,3 +14,18 @@ var singleNumber = function (nums) {
     return ans;
 };
 
+
+
+var singleNumber = function(nums) {
+    const map = {};
+
+    for (let n of nums) {
+        map[n] = (map[n] || 0) + 1;
+    }
+
+    for (let key in map) {
+        if (map[key] === 1) {
+            return Number(key);
+        }
+    }
+};
