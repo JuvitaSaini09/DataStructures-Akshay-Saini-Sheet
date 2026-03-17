58bb3b9e119f003bfbb3c765d9c55f30e4eb2f0b
stutisk <48529714+stutisk@users.noreply.github.com>
2026-03-18 02:10:01 +0530

    Basics of Linkedlist

--- StutiDSA/LinkedList/Basics.js
+++ StutiDSA/LinkedList/Basics.js
@@ -22,3 +22,61 @@ class Node {
 // We will initialize a temporary pointer to the head node of the singly linked list.
 // After that, we will check if that pointer is null or not null, if it is null, then return.
 // While the pointer is not null, we will access and print the data of the current node, then we move the pointer to next node.
+
+class Node {
+
+    // constructor to initialize a new node with data
+    constructor(newData) {
+        this.data = newData;
+        this.next = null;
+    }
+}
+function traverseList(head) {
+
+    while (head !== null) {
+        process.stdout.write(head.data.toString());
+        if (head.next !== null) {   
+            console.log();
+            (" -> ");
+        }
+        head = head.next;
+    }
+    console.log();
+}
+
+function traverseRecursion(head) {
+    if (head === null) return;
+  
+    console.log(head.data);   
+    traverseRecursion(head.next);
+  }
+
+let head = new Node(10);
+head.next = new Node(20);
+head.next.next = new Node(30);
+head.next.next.next = new Node(40);
+
+traverseList(head);
+
+// Output:
+// 10 -> 20 -> 30 -> 40
+
+//  🔹 Traversal of Singly Linked List (Recursive Approach)
+// You are given the head of a singly linked list.
+// Return an array containing the values of the nodes.
+
+
+function LinkedListToArray(){
+    let result = [];
+    let temp = head;
+    while(temp !== null){
+        result.push(temp.data);
+        temp = temp.next;
+    }
+    return result
+}
+
+
+
+
+
