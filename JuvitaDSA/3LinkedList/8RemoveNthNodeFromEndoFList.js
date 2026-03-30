72ca0155cdec9964224d0105fd99850b193dfad1
JuvitaSaini <juvita.saini@nimbusnext.com>
2026-03-30 18:30:37 +0530

    sol 1 for remove nth node in linked list

--- JuvitaDSA/3LinkedList/8RemoveNthNodeFromEndoFList.js
+++ JuvitaDSA/3LinkedList/8RemoveNthNodeFromEndoFList.js
@@ -1,5 +1,32 @@
 // first solution two pass 
 
+// find length to to travel=x=L-n then
+// travel x steps and delete the node ( use prev and curr pointer)
+
+var removeNthFromEnd = function (head, n) {
+    let length = 0;
+    let current = head;
+    if (head === null) return null;
+    while (current != null) {
+        length++;
+        current = current.next;
+    }
+    if (length === n) return head.next;
+    let distanceToTravel = length - n;
+    let prev = null;
+    let temp = head;
+    while (distanceToTravel > 0) {
+        prev = temp;
+        temp = temp.next;
+        distanceToTravel--;
+    }
+
+    prev.next = temp.next;
+    return head;
+
+}
+
+
 var removeNthFromEnd = function (head, n) {
     if (!head) return null;
     let current = head;
