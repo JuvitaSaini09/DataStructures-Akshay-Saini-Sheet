9d805a5d0ebe8e7198ee6eed2cff5b6ca5618e67
Juvita Saini <juvitasaini@gmail.com>
2026-04-18 16:15:23 +0530

    reverse string using two pointers and strings

---
+++ JuvitaDSA/4Strings/14reverse-words-in-a-string.js
@@ -0,0 +1,24 @@
+/**
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+ */
+
+var reverseWords = function (s) {
+    let i = s.length - 1;
+    let result = []
+
+    while (i >= 0) {
+        if (s[i] === " ") {
+            i--;
+            continue;
+        }
+        let j = i;
+        while (s[j] != " " && j >= 0 ) {
+            j--;
+        }
+        result.push(s.slice(j + 1, i + 1));
+        i = j-1;
+    }
+
+    return result.join(" ")
+};
